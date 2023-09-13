268923ba472120cf08a9a90199d337db3192e1fc
Vishesh-Sharma221 <sharmavishesh022@gmail.com>
2023-09-14 01:31:23 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", () => {
+    // Navigate to the quiz page (quiz.html in this example)
+    window.location.href = "quiz.html";
+});
