af99b82c01d274609e6f586418d13201c7f57276
Vishesh-Sharma221 <sharmavishesh022@gmail.com>
2023-09-17 20:29:03 +0530

    Styling changed

--- index.js
+++
@@ -1,6 +0,0 @@
-const startButton = document.getElementById("startButton");
-
-startButton.addEventListener("click", () => {
-    // Navigate to the quiz page (quiz.html in this example)
-    window.location.href = "quiz.html";
-});
